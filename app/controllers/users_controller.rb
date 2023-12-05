e2ba11051f385774ae75323bdb28cbe1e69dc6b7
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2023-12-05 06:55:53 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  
+
   before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def index
     @user = User.find(current_user.id)
     @book_new = Book.new
@@ -33,12 +33,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to books_path
+      redirect_to user_path(current_user.id)
     end
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  get "homes/about" => "homes#about", as: "about"
+  get "home/about" => "homes#about"
   # devise_scope :user do
   #   get '/users/sign_out' => 'devise/sessions#destroy'
   # end
